ca4a28b7a43bcc5e16a42817231dad7ba6272e56
felipeportugalll <felipetpramirez@gmail.com>
2023-02-09 22:05:44 -0300

    Alterações firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import KEY from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1UhhbrxXH5jybKpQPnpP0jNqjsuMSzn0",
-  authDomain: "video-7451b.firebaseapp.com",
-  projectId: "video-7451b",
-  storageBucket: "video-7451b.appspot.com",
-  messagingSenderId: "92709731048",
-  appId: "1:92709731048:web:6271a4b7b6b28c1710e75d"
+        KEY
 };
 
 // Initialize Firebase
